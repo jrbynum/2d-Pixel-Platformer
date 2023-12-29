87c4f8a894fd8a583b10b8752f750bf4b3c62c5e
Rusty <jrbynum@shaw.ca>
2023-12-28 20:28:56 -0700

    add animation playyer and cleaned up animations

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var screen_size = get_viewport_rect().size
+#@onready var screen_size = get_viewport_rect().size
 @onready var sprint_flag : bool = false
 @onready var crouch_flag : bool = false
 @export var SPEED : float = 300.0
@@ -14,45 +14,56 @@ var air_jumps_current : int = air_jumps_total
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 
-@onready var sprite_2d = $AnimatedSprite2D
+@onready var _sprite2d = $Sprite2D
+@onready var _Animation = $AnimationPlayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	#Animations
-	#check left and right velocity to see if we are running or not
+	#check left and right velocity to see if we are running or not also make sure we are not crouched
 	#check to see if sprint is enabled
-	if(velocity.x > 1 || velocity.x < -1):
+	if(not crouch_flag):
+		if(velocity.x > 1 || velocity.x < -1):
 			if sprint_flag :
-				sprite_2d.play("running")
+				_Animation.play("running")
 			else:
-				sprite_2d.play("walking")
+				_Animation.play("walking")
+		else:
+			_Animation.play("idle")
 	else:
-		sprite_2d.play("idle")
+	#we are crouched and play animations accordingly
+		if(velocity.x > 1 || velocity.x < -1):
+			_Animation.play("crawling")
+		else:
+			_Animation.play("crouch")
 		
 	# Check to see if we are on the floor if not add gravity.
 	if not is_on_floor():
 		velocity.y += get_gravity() * delta
 		if velocity.y > 0:
-			sprite_2d.play("falling")
+			_Animation.play("falling")
 	#check for the sprint toggle
 	if Input.is_action_just_pressed("sprint"):
 		sprint_flag = !sprint_flag
+		
+	if Input.is_action_just_pressed("crouch"):
+		crouch_flag = !crouch_flag
 	#change speed based on the sprint flag	
 	if sprint_flag :
 		SPEED = 400
 	else:
 		SPEED = 200
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump"): 
+	# Handle Jump - can't jump if crouched
+	if Input.is_action_just_pressed("jump") and not crouch_flag: 
 		if is_on_floor():
 			jump();
-			sprite_2d.play("jumping")
+			_Animation.play("jump")
 		#velocity.y = jump_velocity
 		if air_jumps_current > 0 and not is_on_floor():
 			air_jump();
-			sprite_2d.play("jumping")
+			_Animation.play("jump")
 	# Get the input direction and handle the movement/deceleration.
 	# Input for the x axis(horizontal) 
 	# Keys A, D, and Left and Right Keyboard Arrow. Also left stick on gamepad.
@@ -70,9 +81,9 @@ func _physics_process(delta):
 	#check for character direction and flip sprite accordingly
 	var isFacing = velocity.x
 	if isFacing < 0:
-		sprite_2d.flip_h = true
+		_sprite2d.flip_h = true
 	if isFacing > 0:
-		sprite_2d.flip_h = false
+		_sprite2d.flip_h = false
 
 func slow_friction(d):
 	velocity.x -= .02 * d
